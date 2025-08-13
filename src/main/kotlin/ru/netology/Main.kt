c8642cdc5e7d9e09436abe2402d192958738eae4
vanya <whitebeltvanya@gmail.com>
2025-08-14 00:59:23 +0300

    Changed algorithm: made checking last digits in val likes

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,7 +1,7 @@
 package ru.netology
 
 fun main() {
-    val likesList = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 99, 1000, 3888, 7_555, 10_000, 1000_000)
+    val likesList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 101, 121, 1911, 7_321, 9_811, 10_000)
     for (likes in likesList) {
         val personStr = getPersonByIf(likes)
         println("Понравилось $likes $personStr")
@@ -9,26 +9,22 @@ fun main() {
 }
 
 fun getPersonByIf(likes: Int): String {
-    val ret = if (likes == 0 ||
-        (likes >= 1000 && likes % 1000 == 0)
-    ) {
-        "человек"
-    } else if (likes == 1) {
+    val ret = if (likes % 10 == 1 && likes % 100 != 11) {
         "человеку"
-    } else if (likes >= 2 && likes <= 4) {
-        "людям"
+//    } else if (likes % 1000 == 0) {
+//        "человек"
     } else {
-        "человекам"
+        "людям"
     }
+
     return ret
 }
 
 fun getPersonByWhen(likes: Int): String {
     val ret = when {
-        (likes == 0 || (likes >= 1000 && likes % 1000 == 0)) -> "человек"
-        likes == 1 -> "человеку"
-        (likes in 2..4) -> "людям"
-        else -> "человекам"
+        (likes % 10 == 1 && likes % 100 != 11) -> "человеку"
+        //(likes % 1000 == 0) -> "человек"
+        else -> "людям"
     }
     return ret
 }
